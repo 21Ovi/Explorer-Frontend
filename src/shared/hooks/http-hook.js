4d56180ff5d66ac5122b7024ef5f4b8ab4e51e13
= <=>
2022-05-26 13:55:28 +0530

    latest

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,54 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequest = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequest.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequest.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+    clearError,
+  };
+};
