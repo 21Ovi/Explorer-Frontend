48f4f94d9a4abbaa7910a5be41047f432bd1d72e
= <=>
2022-05-30 23:59:39 +0530

    latest

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -123,7 +123,12 @@ const Auth = () => {
             />
           )}
           {!isLoginMode && (
-            <ImageUpload center id="image" onInput={inputHandler} />
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image."
+            />
           )}
           <Input
             element="input"
